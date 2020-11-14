e0294b2c936aa78274fde9d16a8fe248e826c714
Landon Holland <landon.holland02@gmail.com>
2020-11-14 11:28:32 -0500

    fix command help message and change name of cog

--- discordBotCog.py
+++ discordBotCog.py
@@ -7,7 +7,7 @@ from collections import deque
 # Cog that hold the bot
 
 
-class discordPlaysCog(commands.Cog):
+class DiscordPlays(commands.Cog):
     def __init__(self):
         self.main_loop.start()
 
@@ -25,12 +25,10 @@ class discordPlaysCog(commands.Cog):
     async def main_loop(self):
         controller.controls_update(self.controlsQueue)
 
-
     # Command to start reading inputs
 
-
     @commands.command(name='start', brief='Starts reading chat for inputs.',
-                help='Stops normal operation and starts reading chat for inputs into the emulator.')
+                      help='Start normal operation and starts reading chat for inputs into the emulator.')
     @commands.has_role('Admin')
     async def start_command(self, ctx):
         if not ctx.message.channel in self.activeChannels:
@@ -38,22 +36,21 @@ class discordPlaysCog(commands.Cog):
             embed.add_field(name='Activated',
                             value='Now listening to chat in `{0}`.'.format(ctx.message.channel.name), inline=False)
             embed.set_author(name='discord-plays',
-                            icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                             icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
             await ctx.send(embed=embed)
             self.activeChannels.append(ctx.message.channel)
         else:
             embed = discord.Embed(title='Controller', color=0xff4055)
-            embed.add_field(name='Error', value='Controller is already active.')
+            embed.add_field(
+                name='Error', value='Controller is already active.')
             embed.set_author(name='discord-plays',
-                            icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                             icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
             await ctx.send(embed=embed)
-
 
     # Command to stop the reading of inputs
 
-
     @commands.command(name='stop', brief='Stops reading chat for inputs.',
-                help='Starts normal operation and stops reading chat for inputs into the emulator.')
+                      help='Stops normal operation and stops reading chat for inputs into the emulator.')
     @commands.has_role('Admin')
     async def stop_command(self, ctx):
         if ctx.message.channel in self.activeChannels:
@@ -61,7 +58,7 @@ class discordPlaysCog(commands.Cog):
             embed.add_field(name='Deactivated',
                             value='Stopped listening to chat.', inline=False)
             embed.set_author(name='discord-plays',
-                            icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                             icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
             await ctx.send(embed=embed)
             self.activeChannels.remove(ctx.message.channel)
         else:
@@ -69,7 +66,7 @@ class discordPlaysCog(commands.Cog):
             embed.add_field(
                 name='Error', value='Controller is currently inactive.')
             embed.set_author(name='discord-plays',
-                            icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                             icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
             await ctx.send(embed=embed)
 
     # on_message function for eventual reading of inputs
@@ -85,4 +82,4 @@ class discordPlaysCog(commands.Cog):
 
 def setup(bot):
     # Add cog to bot
-    bot.add_cog(discordPlaysCog())
+    bot.add_cog(DiscordPlays())
