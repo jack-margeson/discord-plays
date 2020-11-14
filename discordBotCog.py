042091bb944dfbad2014c237ca60b0a230eeca98
Landon Holland <landon.holland02@gmail.com>
2020-11-14 11:42:09 -0500

    Add game specifying functionality

--- discordBotCog.py
+++ discordBotCog.py
@@ -18,6 +18,9 @@ class DiscordPlays(commands.Cog):
         self.controlsDict = controller.load_controls_dict('controls.json')
         self.controlsQueue = deque([])
 
+        # variable to store the active game
+        self.activeGame = ''
+
     def cog_unload(self):
         self.main_loop.cancel()
 
@@ -28,17 +31,32 @@ class DiscordPlays(commands.Cog):
     # Command to start reading inputs
 
     @commands.command(name='start', brief='Starts reading chat for inputs.',
-                      help='Start normal operation and starts reading chat for inputs into the emulator.')
+                      help='Start normal operation and starts reading chat for inputs into the emulator.\nGameName must be inside of quotes.')
     @commands.has_role('Admin')
-    async def start_command(self, ctx):
+    async def start_command(self, ctx, GameName):
         if not ctx.message.channel in self.activeChannels:
-            embed = discord.Embed(title='Controller', color=0x77dd77)
-            embed.add_field(name='Activated',
-                            value='Now listening to chat in `{0}`.'.format(ctx.message.channel.name), inline=False)
-            embed.set_author(name='discord-plays',
-                             icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
-            await ctx.send(embed=embed)
-            self.activeChannels.append(ctx.message.channel)
+            if (not self.activeGame and GameName) or (self.activeGame and not GameName):
+                if not self.activeGame:
+                    self.activeGame = GameName
+                    await ctx.bot.change_presence(status=discord.Status.idle,
+                                                  activity=discord.Game(name=self.activeGame))
+                embed = discord.Embed(title='Controller', color=0x77dd77)
+                embed.add_field(name='Activated',
+                                value='Now listening to chat in `{0}` for the game `{1}`.'.format(ctx.message.channel.name, self.activeGame), inline=False)
+                embed.set_author(name='discord-plays',
+                                 icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                await ctx.send(embed=embed)
+                self.activeChannels.append(ctx.message.channel)
+
+            else:
+                embed = discord.Embed(title='Controller', color=0xff4055)
+                embed.add_field(name='Unkown arguments',
+                                value='If no game is running type `{0}start` followed by the game.\nIf a game is running in another channel, simply type `{0}start`'.format(
+                                    ctx.bot. command_prefix),
+                                inline=False)
+                embed.set_author(name='discord-plays',
+                                 icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
+                await ctx.send(embed=embed)
         else:
             embed = discord.Embed(title='Controller', color=0xff4055)
             embed.add_field(
@@ -61,6 +79,9 @@ class DiscordPlays(commands.Cog):
                              icon_url='https://raw.githubusercontent.com/jack-margeson/discord-plays/master/profile_picture.png')
             await ctx.send(embed=embed)
             self.activeChannels.remove(ctx.message.channel)
+            if not self.activeChannels:
+                self.activeGame = ''
+                await ctx.bot.change_presence(status=discord.Status.idle, activity=discord.Activity())
         else:
             embed = discord.Embed(title='Controller', color=0xff4055)
             embed.add_field(
